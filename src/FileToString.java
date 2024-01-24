4c9066d7d2356bc63cb7827f94670cea99ed4aa5
Marina <marina.moger@gmail.com>
2024-01-24 11:10:37 +0200

    comments

--- src/FileToString.java
+++ src/FileToString.java
@@ -6,13 +6,14 @@ import java.nio.file.Path;
 
 public class FileToString {
     public static void main(String[] args) throws IOException {
+//1 Java1.11
         String filePath = "./src/test.txt";
         String result = Files.readString(Path.of(filePath));
         System.out.println(result);
 
         String content = result + " Demo";
         Files.writeString(Path.of(filePath), content);
-
+//2 Streams Java 1.8
         String result2 = new String(Files.readAllBytes(Path.of(filePath)), StandardCharsets.UTF_8);
         System.out.println(result2);
     }
